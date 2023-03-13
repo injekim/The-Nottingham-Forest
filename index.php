f9815567a64ca75dbb5b72e8f4f05012e3adac71
injekim <inje.k@outlook.com>
2023-03-13 21:52:03 +0000

    Set basic structures

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<meta charset="UTF-8">
-<html>
+<!DOCTYPE html>
+<html lang="en-GB">
 	<head>
+		<meta charset="UTF-8">
 		<title>The Nottingham Forest</title>
+		
+		<!-- Stylesheets -->
+		<link href="global-style.css" rel="stylesheet">
+		
+		<!-- Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Finlandica:wght@400;500;600;700&display=swap" rel="stylesheet">
 	</head>
 	<body>
+		<h1>test</h1>
 	</body>
 </html>
